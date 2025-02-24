7c956e7bdd3580a164184eb331281637c8eef4bf
Mathieu Bridon <mathieu@daitauha.fr>
2025-02-24 16:51:24 +0100

    Add the missing import

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
