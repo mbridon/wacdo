193d318073353cafab7174ffd5b7d77cfeafb765
Mathieu Bridon <mathieu@daitauha.fr>
2025-02-24 10:19:58 +0100

    WIP for the restaurant form

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -2,7 +2,17 @@ from django.forms import ModelForm
 from .models import Restaurant
 
 
-class NewRestaurantForm(ModelForm):
+class RestaurantForm(ModelForm):
     class Meta:
         model = Restaurant
         fields = ["name", "address", "post_code", "city"]
+
+    def save(self, args, **kwargs):
+        restaurant = super().save(*args, **kwargs)
+        print(self.cleaned_data)
+        print(restaurant)
+        #password = self.cleaned_data['password']
+        #user.set_password(password)
+        #user.is_staff = True
+        #user.save()
+        #return user
