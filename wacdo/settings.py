d1efc091715572eb8898520745d408e5f0e9fea7
gducampus <gducampus@gmail.com>
2025-05-07 00:21:34 +0200

    first commit

--- wacdo/settings.py
+++ wacdo/settings.py
@@ -56,9 +56,8 @@ WSGI_APPLICATION = "wacdo.wsgi.application"
 
 # Database via Railway
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
+    'default': dj_database_url.parse(
+        os.getenv("DATABASE_URL", ""), conn_max_age=600
     )
 }
 
